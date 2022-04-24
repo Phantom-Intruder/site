914cc6e93bb4042055e2c0f7d6496ea5a9683639
You <you@mewantha.net>
2022-04-24 12:10:56 +0530

    Filled frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormComponent } from './form/form.component';
-import { AboutComponent } from './about/about.component';
 import { CommitFormComponent } from './commit-form/commit-form.component';
 import { CommitsComponent } from './commits/commits.component';
 import { HttpErrorHandler } from './http-error-handler.service';
 import { MessageService } from './message.service';
+import { SiteBodyComponent } from './site-body/site-body.component';
+import { BodyTextComponent } from './body-text/body-text.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FormComponent,
-    AboutComponent,
     CommitFormComponent,
-    CommitsComponent
+    CommitsComponent,
+    SiteBodyComponent,
+    BodyTextComponent,
+    TechnologiesComponent
   ],
   imports: [
     BrowserModule,
